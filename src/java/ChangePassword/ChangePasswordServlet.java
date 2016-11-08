fca5eb55fe524af61cd5378479ee0f2e417b47e9
mperez5 <marco.perez@RaymondJames.com>
2016-11-08 09:48:10 -0500

    Updated ChangePasswordServlet - Now has getAttribute Updated Account_activity.jsp - Now displayed user attributes

--- src/java/ChangePassword/ChangePasswordServlet.java
+++ src/java/ChangePassword/ChangePasswordServlet.java
@@ -1,13 +1,13 @@
 package ChangePassword;
 
 import User.User;
-import User.User;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet(urlPatterns = {"/changePassword"})
 public class ChangePasswordServlet extends HttpServlet {
@@ -23,29 +23,28 @@ public class ChangePasswordServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
+        //Getting a session
+        HttpSession session = request.getSession();
+        //Getting user from session
+        User user = (User) session.getAttribute("user");
+        //If user doesn't exist, create one
+        if(user == null)
+        {
+            user = new User();       
+        }
+        
         String url = "/Account_activity.jsp";
-        
-        
-        String fName = request.getParameter("myFName");
-        String lName = request.getParameter("myLName");
-        String eMail = request.getParameter("myEmail");
-        String phone = request.getParameter("myPhone");
-        String address = request.getParameter("address");
-        String city = request.getParameter("city");
-        String state = request.getParameter("state");
-        String zip = request.getParameter("zip");        
+             
+        //Get the username and password from the form
         String userName = request.getParameter("userName");
         String password = request.getParameter("password");
         
-        
+        //Change the password on the user object
+        user.setPassword(password);
         
         String message = "";
         
-        User user = new User(fName, lName, eMail, phone, address, city, state, zip, userName, password);
-        request.setAttribute("user", user);
-        //user.setPassword(password);
-        
-        
+        //Will redirect back to reset password page if any fields are blank
         if(userName.isEmpty() || password.isEmpty())
         {
             //Direct back to form if any fields are blank.
@@ -53,17 +52,12 @@ public class ChangePasswordServlet extends HttpServlet {
             url = "/passwordReset.jsp";   
         }
         
+        //Error message to be displayed if needed
         request.setAttribute("message", message);
-        request.setAttribute("fName", fName);
-        request.setAttribute("lName", lName);
-        request.setAttribute("eMail", eMail);
-        request.setAttribute("phone", phone);
-        request.setAttribute("address", address);
-        request.setAttribute("city", city);
-        request.setAttribute("state", state);
-        request.setAttribute("zip", zip);
-        request.setAttribute("userName", userName);
-        request.setAttribute("password", password);
+
+        //Setting session attributes
+        session.setAttribute("userName", userName);
+        session.setAttribute("password", password);
         
         getServletContext()
             .getRequestDispatcher(url)
--- web/Account_activity.jsp
+++ web/Account_activity.jsp
@@ -8,17 +8,16 @@
 <c:import url="/includes/navigation.html" />
 
 <main>
-<h2>Banking made easy, register today!</h2>
+<h2>Account Activity</h2>
 <img src="styles/bank.jpg" alt="bank vault" width="320" height="250">
 <p><span class="resort">Titan Banks</span> online service offers a modern banking experience on our Hi-Tech application developed in house.
-  Login or create an online profile today to start using the Titan Online Banking Application.</p>
-<ul>
-  <li>View transactions in real time</li>
-  <li>View monthly statements</li>
-  <li>Transfer money between accounts</li>
-  <li>Request new debit cards</li>
-  <li>Dispute a charge with ease</li>
-</ul>
+  This is a place holder for the Account Activity Page.</p>
+<p>
+<span>First Name: ${user.firstName}<br></span>
+<span>Last Name: ${user.lastName}<br></span>
+<span>Email: ${user.email}<br></span>
+<span>Phone: ${user.phone}<br><br></span>
+</p>
   <div class="clear">Titan Bank<br>
         5555 Limerick Drive<br>
         Tampa, FL &nbsp;33610<br>
